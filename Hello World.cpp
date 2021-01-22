dec083cd588c8ff43590af68e88fc1a4099aeb82
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2021-01-21 23:27:16 -0600

    Update Hello World.cpp

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "Hello, World!";//基本语法
+    std::cout << "Hello, World!" << endl;
+    cout << "Hello, World!";//需要using namespace std，否则只能使用上行及上上行代码
+    cout << "Hello, World!" << endl;
+    system("echo Hello, World!");//仅Windows系统
+    printf("Hello, World");//需要stdio.h
 }
