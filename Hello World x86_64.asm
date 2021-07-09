a20eab57a9a7618500514e89e27765407c818590
crazy_piggy_ <57442208+crazy-piggy@users.noreply.github.com>
2021-07-09 08:19:13 +0800

    Create Hello World x86_64.asm

---
+++ Hello World x86_64.asm
@@ -0,0 +1,39 @@
+.LC0:
+        .string "Hello, World!"
+Main():
+        push    rbp
+        mov     rbp, rsp
+        mov     esi, OFFSET FLAT:.LC0
+        mov     edi, OFFSET FLAT:_ZSt4cout
+        call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
+        nop
+        pop     rbp
+        ret
+__static_initialization_and_destruction_0(int, int):
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     DWORD PTR [rbp-4], edi
+        mov     DWORD PTR [rbp-8], esi
+        cmp     DWORD PTR [rbp-4], 1
+        jne     .L4
+        cmp     DWORD PTR [rbp-8], 65535
+        jne     .L4
+        mov     edi, OFFSET FLAT:_ZStL8__ioinit
+        call    std::ios_base::Init::Init() [complete object constructor]
+        mov     edx, OFFSET FLAT:__dso_handle
+        mov     esi, OFFSET FLAT:_ZStL8__ioinit
+        mov     edi, OFFSET FLAT:_ZNSt8ios_base4InitD1Ev
+        call    __cxa_atexit
+.L4:
+        nop
+        leave
+        ret
+_GLOBAL__sub_I_Main():
+        push    rbp
+        mov     rbp, rsp
+        mov     esi, 65535
+        mov     edi, 1
+        call    __static_initialization_and_destruction_0(int, int)
+        pop     rbp
+        ret
